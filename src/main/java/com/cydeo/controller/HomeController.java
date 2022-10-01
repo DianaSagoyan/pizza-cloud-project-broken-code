8e7c2ac1d89eefe6f8a9ec1f74ee54e4eb2e173d
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-01 16:48:32 -0500

    Working on design page

--- src/main/java/com/cydeo/controller/HomeController.java
+++ src/main/java/com/cydeo/controller/HomeController.java
@@ -1,10 +1,22 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
+import com.cydeo.model.Pizza;
+import com.cydeo.repository.PizzaRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    private final PizzaRepository pizzaRepository;
+    private final Pizza pizza;
+
+    public HomeController(PizzaRepository pizzaRepository, Pizza pizza) {
+        this.pizzaRepository = pizzaRepository;
+        this.pizza = pizza;
+    }
     @GetMapping
     public String homePage(){
 
@@ -12,7 +24,13 @@ public class HomeController {
     }
 
     @GetMapping("/design")
-    public String designPage(){
+    public String designPage(Model model){
+
+        model.addAttribute("pizza", pizza);
+        model.addAttribute("cheeses", DataGenerator.cheeseTypeList);
+        model.addAttribute("sauces", DataGenerator.sauceTypeList);
+        model.addAttribute("toppings", DataGenerator.toppingTypeList);
+
         return "/design";
     }
 }
